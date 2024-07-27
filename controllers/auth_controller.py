89bb40f2651d777e08b820b69b61a78c150939be
Stephh-E <steph_elsley@hotmail.com>
2024-07-27 18:21:41 +1000

    changes to auth and addition of other controllers

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
 from psycopg2 import errorcodes
+
 from init import bcrypt, db
 from models.user import User, user_schema
 
@@ -11,7 +15,7 @@ def register_user():
     try:
         # get the data from the body of the request
         body_data = request.get_json()
-        # create an instance of the User model
+        # create an instance of the user model
         user = User(
             name=body_data.get("name"),
             email=body_data.get("email")
@@ -30,12 +34,27 @@ def register_user():
 
         # respond back
         return user_schema.dump(user), 201
+    
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {"error": f"The coloumn {err.orig.diag.column_name} is required"}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error":"Email address already in use"}, 409
 
+@auth_bp.route("/login", methods=["POST"])
+def auth_login():
+    # get the data data from the body of the request
+    body_data = request.json()
+    #find the user in the db with that email address
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    #if user exists and password is correct
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # create jwt
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {"email": user.email, "token": token, "is_admin": user.is_admin}
+    else:
+        return {"error": "Invalid email or password"}, 401
 
 
 
---
+++ controllers/wishlist_controller.py
@@ -0,0 +1,52 @@
+
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from init import db
+from models.book import Book
+from models.wishlist import Wishlist, wishlist_schema
+
+wishlists_bp = Blueprint('wishlists', __name__, url_prefix="/<int:book_id>/wishlists")
+    
+@wishlists_bp.route("/", methods=["POST"])
+@jwt_required()
+def create_wishlist(book_id):
+    body_data = request.get_json()
+    stmt = db.select(Book).filter_by(id=book_id)
+    book = db.session.scalar(stmt)
+    if book:
+        wishlist = Wishlist(
+            wishlist_title = body_data.get('message'),
+            user_id = get_jwt_identity(),
+            book = book
+        )
+        db.session.add(wishlist)
+        db.session.commit()
+        return wishlist_schema.dump(wishlist), 201
+    else:
+        return {"error": f"Book with id {book_id} doesn't exist"}, 404
+    
+@wishlists_bp.route("/<int:wishlist_id>", methods=["DELETE"])
+@jwt_required()
+def delete_wishlist(book_id, wishlist_id):
+    stmt = db.select(Wishlist).filter_by(id=wishlist_id)
+    wishlist = db.session.scalar(stmt)
+    if wishlist and wishlist.book.id == book_id:
+        db.session.delete(wishlist)
+        db.session.commit()
+        return {"message": f"Wishlist with id {wishlist_id} has been deleted"}
+    else:
+        return {"error": f"Wishlist with id {wishlist_id} not found in book with id {wishlist_id}"}, 404
+    
+@wishlists_bp.route("/<int:wishlist_id>", methods=["PUT", "PATCH"])
+@jwt_required()
+def edit_wishlist(book_id, wishlist_id):
+    body_data = request.get_json()
+    stmt = db.select(Wishlist).filter_by(id=wishlist_id, book_id=book_id)
+    book = db.session.scalar(stmt)
+    if book:
+        book.title = body_data.get('title') or book.title
+        db.session.commit()
+        return wishlist_schema.dump(wishlist)
+    else:
+        return {"error": f"Wishlist with id {wishlist_id} not found in movie with id {book_id}"}
